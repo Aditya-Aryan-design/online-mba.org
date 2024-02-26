d94542795a7c6a49d4a8073571f6fd8c60fa8c3b
Aditya-Aryan-design <113804917+Aditya-Aryan-design@users.noreply.github.com>
2024-02-26 21:27:49 +0530

    initial commit

--- app/(main)/connect/page.tsx
+++ app/(main)/connect/page.tsx
@@ -118,7 +118,7 @@ const page = () => {
 
         <div className="p-2 w-1/2">
           <div className="relative">
-            <label htmlFor="univercity" className="leading-7 text-sm text-gray-600">Univercity</label>
+            <label htmlFor="univercity" className="leading-7 text-sm text-gray-600">University</label>
             <select id="univercity" name="univercity" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-orange-600 focus:bg-white focus:ring-2 focus:ring-orange-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             
             value={univercity}
